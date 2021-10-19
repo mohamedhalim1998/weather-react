3fa1b58d661fc62f82d8ec9d8b0d3e43f717a5cb
MohamedHalim <mohamedhalim1998@gmail.com>
2021-10-19 19:50:47 +0200

    ADD: implement search functions

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import "../css/search-box.css";
+import { useContext } from "react";
+import { weatherActions, WeatherContext } from "../context/WeatherContext";
+import WeatherService from "../services/WeatherService";
+
 function SearchBox() {
   const [city, setCity] = useState("");
+  const { data, dispatch } = useContext(WeatherContext);
   const changeCity = ({ target: input }) => {
     setCity(input.value);
   };
+  const searchCity = async (e) => {
+    e.preventDefault();
+    dispatch({ type: weatherActions.searchCity, payload: city });
+    const data = await WeatherService.getWeather(city);
+    dispatch({ type: weatherActions.getWeather, payload: data });
+  };
   return (
-    <form id="search-box">
+    <form id="search-box" onSubmit={searchCity}>
       <div className="form-input">
         <input
           type="text"
@@ -17,7 +28,7 @@ function SearchBox() {
           onChange={changeCity}
         ></input>
         <span>
-          <i className="fa fa-search"></i>
+          <i className="fa fa-search" onClick={searchCity}></i>
         </span>
       </div>
     </form>
